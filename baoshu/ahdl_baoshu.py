61eb8e48eaa5e8fba8d17f9dcf4cee53bd17d7b8
ganyuanrong <372129081@qq.com>
2017-07-31 17:13:15 +0800

    添加暗黑大陆ltv

--- baoshu/ahdl_baoshu.py
+++ baoshu/ahdl_baoshu.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 from baoshu import time_helper
 from baoshu.AccountLogin import Login
 from baoshu.ServerMsg import ServerMsg
+from baoshu.smsgtool import sumSmsg
 from excel.excelutil import writeExcelForGameInfo
 
 
@@ -36,7 +37,7 @@ def getAhdlData():
     servers_map = getServersMap(login_session) #获取所有服务器idhe名称
 
     sMsgArray_Android = getSMsgList(login_session, servers_map,'50100')#android
-    sMsgArray_Ios = getSMsgList(login_session, servers_map,'50103')#android
+    sMsgArray_Ios = getSMsgList(login_session, servers_map,'50103')#IOS
 
     for smAndroid in sMsgArray_Android:
         for smIos in sMsgArray_Ios:
@@ -52,12 +53,8 @@ def getAhdlData():
                 if smAndroid.roleLogin > 0:
                     smAndroid.payPercent = str(round((smAndroid.totalRolePay / float(smAndroid.roleLogin)) * 100, 2)) + '%'
 
-
-    writeExcelForGameInfo('E:\\jingling\\ahdl_baoshu.xls', u'暗黑邊境 %s' % (time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))), sMsgArray_Android)
-
-
-
-
+    listSmsg = sumSmsg(sMsgArray_Android)
+    writeExcelForGameInfo('E:\\jingling\\ahdl_baoshu.xls', u'暗黑邊境 %s' % (time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))), listSmsg)
 
 
 def getSMsgList(login_session, servers_map, channelId):
@@ -110,8 +107,49 @@ def getSMsgList(login_session, servers_map, channelId):
                                 # sMsg.payPercent = tds[19]
                                 print td.string
                             sMsg_lsit.append(sMsg)
+
+                if channelId == '50100' and sMsg:
+                    payment_1, newRole_1 = getSMsgLTVData(login_session,sId,'50100')
+                    payment_2, newRole_2 = getSMsgLTVData(login_session,sId,'50103')
+                    if (newRole_1 + newRole_2) > 0:
+                        ltv = round((payment_1 + payment_2) / (newRole_1 + newRole_2), 2)
+                        sMsg.ltv = ltv
+
     return sMsg_lsit
 
+
+def getSMsgLTVData(login_session, sId, channelId):
+
+    mHeaders = {
+        'Referer': 'http://ahdltw-serlist.starb168.com:8188/longshuai/payment/payment-synopsis-statistics'
+    }
+    mValues = {
+        'serverId': sId,
+        'channelId': channelId,
+        'startDate': '2017-07-29',
+        'endDate': time_helper.get_tomorrow_time()
+    }
+    serverData = login_session.post(
+        'http://ahdltw-serlist.starb168.com:8188/longshuai/payment/payment-synopsis-statistics', headers=mHeaders,
+        data=mValues)
+    if serverData:
+        # print serverData.text
+        pageText = serverData.text
+        # pageText = pageText.replace('\n','')
+        soup_s_data = BeautifulSoup(pageText, 'html.parser')
+        if soup_s_data:
+            tr_list = soup_s_data.find_all('tr')
+            for tr in tr_list:
+                tds = tr.contents
+                if tds:
+                    theServerMsgAllText = tds[1].string
+                    if theServerMsgAllText == u'总计':
+                        payment = float(tds[9].string)
+
+                        payment = round(payment / 32, 2)
+                        newRole = int(tds[15].string)
+                        return payment, newRole
+    return 0,0
 
 def getServersMap(login_session):
     mHeaders = {
